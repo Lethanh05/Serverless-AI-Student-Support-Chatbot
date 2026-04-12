9ab6a9a07e8d23d5b657d78463d87a54fbe1633c
Hữu Duy <duyyyyyy98712@gmail.com>
2026-04-13 04:31:19 +0700

    Duy cap nhat giao dien Chatbot

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root')).render( // Sử dụng StrictMode để phát hiện các vấn đề tiềm ẩn trong ứng dụng, giúp cải thiện chất lượng mã nguồn và đảm bảo tuân thủ các best practices của React
   <StrictMode>
     <App />
   </StrictMode>,
